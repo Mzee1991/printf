a433150514fd32d05f08c49940a5f27161d9c1c8
Batya Tonny <batztonnie@gmail.com>
2022-04-19 11:42:25 +0300

    Small Change

--- print_alpha.c
+++ print_alpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_string - loops through a string and prints
+ * print_string - loops through a  string and prints
  * every character
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
@@ -10,7 +10,7 @@
  */
 int print_string(va_list l, flags_t *f)
 {
-	char *s = va_arg(l, char *);
+	char *s = va_arg(l, char  *);
 
 	(void)f;
 
