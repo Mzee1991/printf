1500bc9f79d0523587cdbdf1e9b9b3ed5e478ed0
Mzee1991 <yohanamariamzee@gmail.com>
2022-04-21 11:53:10 -0700

    just one chnge

--- params.c
+++ params.c
@@ -18,7 +18,7 @@ void init_params(params_t *params, va_list ap)
 	params->minus_flag = 0;
 
 	params->width = 0;
-	params->precision = UINT_MAX;
+	params->precision = -2;
 
 	params->h_modifier = 0;
 	params->l_modifier = 0;
